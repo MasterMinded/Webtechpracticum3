a45224c9e4adcbef16953958f5be5b9cc73bf94b
MasterMinded <robinl33t@hotmail.com>
2016-04-24 13:03:05 +0200

    testmodel toegevoegd om database te testen

---
+++ application/models/Datatestv3.php
@@ -0,0 +1,13 @@
+<?php
+
+Class Datatestv3 extends CI_Model {
+
+
+    public function getData()
+    {
+
+        $testquery = $this->db->query("SELECT * FROM Users");
+        return $testquery->result();
+    }
+}
+?>
